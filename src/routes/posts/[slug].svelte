c4a5e46985dde6a2a951e43551599de14f7abdea
wxllow <willow@wxllow.dev>
2022-06-28 10:32:57 -0400

    Update posts CSS

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -9,11 +9,12 @@
     <title>{post.metadata.title}</title>
 </svelte:head>
 
-<h1 class="title text-5xl">{post.metadata.title}</h1>
-<p class="info text-gray-700 dark:text-gray-300">Published {post.metadata.date}</p>
-<div class="w-full border-b border-black dark:border-white "></div>
 
-<post class="px-20 postcontent">
+
+<post class="px-20">
+    <h1 class="title text-5xl">{post.metadata.title}</h1>
+    <p class="info text-gray-700 dark:text-gray-300">Published {post.metadata.date}</p>
+    <div class="w-full border-b border-black dark:border-white "></div>
     {#if post.metadata.video }
         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/{post.metadata.video}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
     {/if}
