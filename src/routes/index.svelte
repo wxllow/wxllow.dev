9c2f410477c89ce5f6d5cb65b3266eefca7ff3d8
wxllow <willow@wxllow.dev>
2022-06-29 21:02:23 -0400

    Sus

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <h1 class="text-5xl font-bold sidelines">wxllow.dev</h1>
 <p>Teenage crewmate with an interest for programming, AmogOS, & playing AmongUs.</p>
-<p>I code mainly in ImpostorScript, Polus, and JavaSloth</p>
+<p>I code mainly in ImpostorScript, Polus, and JavaSloth ඞ</p>
 <div class="text-center">
     <a href="https://github.com/wxllow">
         <img alt="GitHub Statistics" src="/github-metrics.svg" />
