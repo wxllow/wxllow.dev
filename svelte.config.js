2d1e9ada9aa4f8ec985327af2210e2a5b81c15e4
wxllow <willow@wxllow.dev>
2022-09-15 19:44:35 -0400

    Update svelte pt 1

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            out: "build",
-        }),
-    },
-
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: [
-        preprocess({
-            postcss: true,
-        }),
-    ],
+    preprocess: preprocess({ postcss: true }),
+
+    kit: {
+        adapter: adapter(),
+    },
 };
 
 export default config;
