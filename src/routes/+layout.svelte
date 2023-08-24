d54a763e371306e2f90db7a1a8beb45be497f051
wxllow <willow@wxllow.dev>
2023-08-24 14:29:52 -0400

    Initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import "../app.css";
+    import _ from "radash";
+    import Header from "$components/header.svelte";
+    import Footer from "$components/footer.svelte";
+    import { page } from "$app/stores";
+
+    $: metadata = $page.data.metadata || {
+        title: "",
+        description: "",
+    };
+    $: title = _.trim(metadata.title)
+        ? `${metadata.title} - wxllow.dev`
+        : "wxllow.dev";
+    $: path = $page.url.pathname;
+    $: website = process.env.PUBLIC_URL || "https://wxllow.dev";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="title" content={title} />
+    <meta name="description" content={metadata.description} />
+
+    <!-- Open Graph -->
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={metadata.description} />
+    <meta property="og:url" content={`${website}${path}`} />
+
+    {#if !metadata.type}
+        <meta property="og:type" content="website" />
+    {/if}
+
+    <!-- Tags -->
+    {#each ["code", "dev", "developer", "coding", "programming"] as tag}
+        <meta name="keywords" content={tag} />
+    {/each}
+
+    <!-- Twitter -->
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={metadata.description} />
+    <meta property="twitter:url" content={`${website}${path}`} />
+    <meta property="twitter:domain" content={website} />
+
+    <meta name="theme-color" content="#F4B7C1" />
+</svelte:head>
+
+<div>
+    <header>
+        <Header />
+    </header>
+
+    <main>
+        <slot />
+    </main>
+
+    <footer><Footer /></footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-center">
+    <h1 class="text-5xl font-bold m-4">wxllow.dev</h1>
+    <p class="text-xl m-2">
+        Hey there, welcome to my little corner of the internet!
+    </p>
+    <p class="m-2">I am a programmer, tech enthusiast, and a student.</p>
+    <p class="m-2">
+        I code mostly in TypeScript and Python, but I am also familiar with a
+        few other languages.
+    </p>
+    <!-- Social Links Go Here -->
+</div>
