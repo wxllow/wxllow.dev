675b74fa3c21af211d3177516acfa58f0679d6c8
wxllow <willow@wxllow.dev>
2023-10-06 19:08:32 -0400

    Uses section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,41 @@
-<div class="text-center">
-    <h1 class="text-5xl font-bold m-4">wxllow.dev</h1>
-    <p class="text-xl m-2">
-        Hey there, welcome to my little corner of the internet!
-    </p>
-    <p class="m-2">I am a programmer, tech enthusiast, and a student.</p>
-    <p class="m-2">
-        I code mostly in TypeScript and Python, but I am also familiar with a
-        few other languages.
-    </p>
-    <!-- Social Links Go Here -->
+<script>
+    import Uses from "./uses.component.svelte";
+</script>
+
+<div class="mx-4 my-2 space-y-8 md:container">
+    <div>
+        <h1 class="text-5xl font-bold my-4">wxllow.dev</h1>
+
+        <p class="text-xl">
+            Hey there, welcome to my little corner of the internet!
+        </p>
+        <p class="my-2">I am a programmer and tech enthusiast.</p>
+        <p class="my-2">
+            I code mostly in TypeScript, Go, & Python, but I am also familiar
+            with other languages.
+        </p>
+    </div>
+    <div class="alert alert-warning">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            /></svg
+        >
+
+        <span>This website is a work in progress</span>
+    </div>
+    <div id="blog">
+        <h2 class="text-4xl font-bold my-4">Blog</h2>
+        <p class="text-xl">Coming soon! :)</p>
+    </div>
+    <div id="use">
+        <Uses />
+    </div>
 </div>
---
+++ src/routes/uses.component.svelte
@@ -0,0 +1,170 @@
+<h2 class="text-4xl font-bold my-4">What I Use</h2>
+<h3 class="text-2xl font-bold my-2">Desktop Software</h3>
+<p class="text-lg mb-2">
+    You may
+    <a
+        class="link-accent"
+        href="https://git.wxllow.dev/wxllow/dotfiles"
+        target="_blank">view my dotfiles here</a
+    >.
+</p>
+<ul class="list-disc space-y-2 mb-4">
+    <li>
+        <span class="font-bold">Operating System</span>: I primarily use
+        <a class="link-accent" href="https://archlinux.org/" target="_blank"
+            >Arch Linux</a
+        >
+        because of its customizability, bleeding-edge packages, and the
+        <a class="link-accent" href="https://aur.archlinux.org/" target="_blank"
+            >AUR</a
+        >. I also recommend
+        <a class="link-accent" href="https://endeavouros.com/" target="_blank"
+            >EndeavourOS</a
+        >, an easy to setup Arch-based distro! For server use, I recommend
+        Fedora, Debian, or Ubuntu.
+    </li>
+    <li>
+        <span class="font-bold">Window Manager</span>: I use
+        <a class="link-accent" href="https://hyprland.org/" target="_blank"
+            >Hyprland</a
+        >, a beautiful, easily customizable Wayland window manager. You can find
+        my configuration
+        <a
+            class="link-accent"
+            href="https://git.wxllow.dev/wxllow/dotfiles"
+            target="_blank">here</a
+        >.
+    </li>
+    <li>
+        <span class="font-bold">Terminal</span>:
+        <a
+            class="link-accent"
+            href="https://sw.kovidgoyal.net/kitty/"
+            target="_blank">Kitty</a
+        >
+    </li>
+    <li>
+        <span class="font-bold">Browser</span>: I use
+        <a
+            class="link-accent"
+            href="https://github.com/ungoogled-software/ungoogled-chromium"
+            target="_blank">Ungoogled Chromium</a
+        >
+        as it is a full-fledged Chromium browser, but without the Google spyware.
+        I use a few extensions, notably
+        <a class="link-accent" href="https://ublockorigin.com/" target="_blank"
+            >uBlock Origin</a
+        >,
+        <a
+            class="link-accent"
+            href="https://chrome.google.com/webstore/detail/decentraleyes/ldpochfccmkkmhdbclfhpagapcfdljkj"
+            target="_blank">Decentraleyes</a
+        >,
+        <a class="link-accent" href="https://vimium.github.io/" target="_blank"
+            >Vimium</a
+        >, and
+        <a class="link-accent" href="https://privacybadger.org/" target="_blank"
+            >Privacy Badger</a
+        >
+    </li>
+    <li>
+        <span class="font-bold">Text/Code Editor</span>: I use
+        <a
+            class="link-accent"
+            href="https://code.visualstudio.com/"
+            target="_blank">VSCode</a
+        >
+        (
+        <a class="link-accent" href="https://vscodium.com/" target="_blank"
+            >VSCodium</a
+        >
+        ) and
+        <a class="link-accent" href="https://neovim.io/" target="_blank"
+            >Neovim</a
+        >
+        as my editors of choice. For neovim, I use a configuration based on
+        <a
+            class="link-accent"
+            href="https://github.com/nvim-lua/kickstart.nvim"
+            target="_blank">kickstart.nvim</a
+        >. On both editors, I use the
+        <a class="link-accent" href="https://codeium.com/" target="_blank"
+            >Codeium</a
+        > AI code completion plugin, it is a free alternative to Github Copilot which
+        is only trained on permissive code
+    </li>
+    <li>
+        <span class="font-bold">File Manager</span>: Dolphin
+    </li>
+    <li>
+        <span class="font-bold">Image Editors</span>:
+        <a class="link-accent" href="https://krita.org/" target="_blank"
+            >Krita</a
+        >
+        for digital drawing,
+        <a class="link-accent" href="https://inkscape.org/" target="_blank"
+            >Inkscape</a
+        > for vector graphics
+    </li>
+    <li>
+        <span class="font-bold">Video/audio player</span>:
+        <a class="link-accent" href="https://mpv.io/" target="_blank">mpv</a>
+    </li>
+</ul>
+<h3 class="text-2xl font-bold my-2">Development SaaS/PaaS</h3>
+<ul class="list-disc space-y-2">
+    <li>
+        <span class="font-bold">Web Framework</span>:
+        <a class="link-accent" href="https://nextjs.org/" target="_blank"
+            >Next.JS</a
+        >
+        or
+        <a class="link-accent" href="https://kit.svelte.dev/" target="_blank"
+            >SvelteKit</a
+        >
+        (what this site uses). For static sites, I recommend
+        <a class="link-accent" href="https://gohugo.io/" target="_blank">Hugo</a
+        >
+    </li>
+    <li>
+        <span class="font-bold">Serverless Platform</span>:
+        <a class="link-accent" href="https://vercel.com/" target="_blank"
+            >Vercel</a
+        >
+        or
+        <a
+            class="link-accent"
+            href="https://www.flightcontrol.dev/"
+            target="_blank">Flightcontrol</a
+        >
+    </li>
+    <li>
+        <span class="font-bold">Serverless Database</span>:
+        <a
+            class="link-accent"
+            href="https://www.cockroachlabs.com/"
+            target="_blank">Cockroach DB</a
+        >
+        for relational (PostgresQL) and
+        <a class="link-accent" href="https://upstash.com/" target="_blank"
+            >Upstash</a
+        > for Redis
+    </li>
+    <li>
+        <span class="font-bold">VPS (Virtual Private Server)</span>: I use and
+        recommend
+        <a
+            class="link-accent"
+            href="https://www.vultr.com/?ref=9070089-8H"
+            target="_blank">Vultr</a
+        >
+        . Using my referral link, you can get $100 in credits for 14 days to try
+        out Vultr.
+    </li>
+    <li>
+        <span class="font-bold">Payment Processing</span>:
+        <a class="link-accent" href="https://stripe.com/" target="_blank"
+            >Stripe</a
+        >
+    </li>
+</ul>
