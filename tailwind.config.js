6144556c75941705055333848a18468ab177cbc6
wxllow <willow@wxllow.dev>
2023-10-06 19:08:08 -0400

    Custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,41 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
+
+const themeCustomization = {
+    "primary": "#6a92e2",
+            
+    "secondary": "#cc2874",
+             
+    "accent": "#e88dda",
+ 
+ 
+    "neutral": "#212121",
+}
 export default {
     content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
         extend: {},
+        container: {
+            center: true,
+        },
     },
     plugins: [require("daisyui")],
     daisyui: {
-        themes: ["business", "light"],
+        themes: [ {
+            dark: {
+                ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+                ...themeCustomization,
+                "base-100"   : "#131313",
+
+
+            },
+          }, {
+            light:
+            {
+                ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+                ...themeCustomization,
+                "accent": "#a13891"
+            }
+          }],
     },
 };
