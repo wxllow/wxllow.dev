e6beb08e6b105de625f85d59f3359684afcca775
wxllow <willow@wxllow.dev>
2023-10-06 22:04:06 -0400

    Fix adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { mdsvex, escapeSvelte } from "mdsvex";
 import shiki from "shiki";
